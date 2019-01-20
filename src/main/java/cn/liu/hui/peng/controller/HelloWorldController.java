3a6cb7da33381471123c9c4cbef37fdb73f6bdd4
Liuhp534 <617456341@qq.com>
2019-01-20 22:02:45 +0800

    springmvc学习一

--- src/main/java/cn/liu/hui/peng/controller/HelloWorldController.java
+++ src/main/java/cn/liu/hui/peng/controller/HelloWorldController.java
@@ -1,7 +1,12 @@
 package cn.liu.hui.peng.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 /**
  * @description: hello controller
@@ -10,7 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping(path="helloWorld")
-public class HelloWorldController {
+public class HelloWorldController implements EnvironmentAware {
+
+    private final Log logger = LogFactory.getLog(getClass());
+
+    private Environment environment;
     
     /**
     * @Description: 
@@ -25,7 +34,13 @@ public class HelloWorldController {
     */
     @RequestMapping(path="test")
     public String test() {
-
+        logger.info("处理中。。。。。。");
+        System.out.println("good job!!!!");
         return "helloWorld";
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 }
